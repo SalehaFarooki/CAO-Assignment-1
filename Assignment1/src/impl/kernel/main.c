3423887cde5d0ebff2c236aacee9586281ad198e
SalehaFarooki <67058974+SalehaFarooki@users.noreply.github.com>
2021-04-19 12:18:30 +0500

    Update main.c

--- Assignment1/src/impl/kernel/main.c
+++ Assignment1/src/impl/kernel/main.c
@@ -1,43 +1,41 @@
 #include "print.h"
 
-// Number of lines for the alphabet's pattern
-int height = 5;
-// Number of character width in each line
-int width = (2 * 5) - 1;
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     
+    // Number of lines for the alphabet's pattern
+    int h = 5;
 
     //PRINTING N
-    int i, j, counter = 0;
-    for (i = 0; i < height; i++) {
+    int i, j, c = 0;
+    for (i = 0; i < h; i++) {
         print_str("*");
-        for (j = 0; j <= height; j++) {
-            if (j == height)
+        for (j = 0; j <= h; j++) {
+            if (j == h)
                 print_str("*");
-            else if (j == counter)
+            else if (j == c)
                 print_str("*");
             else
                 print_str(" ");
         }
-        counter++;
+        c++;
         print_str("\n");
     }
 
     //PRINTING U
-    for (i = 0; i < height; i++) {
-        if (i != 0 && i != height - 1)
+    for (i = 0; i < h; i++) {
+        if (i != 0 && i != h - 1)
             print_str("*");
         else
             print_str(" ");
-        for (j = 0; j < height; j++) {
-            if (((i == height - 1)
+        for (j = 0; j < h; j++) {
+            if (((i == h - 1)
                  && j >= 0
-                 && j < height - 1))
+                 && j < h - 1))
                 print_str("*");
-            else if (j == height - 1 && i != 0
-                     && i != height - 1)
+            else if (j == h - 1 && i != 0
+                     && i != h - 1)
                 print_str("*");
             else
                 print_str(" ");
@@ -46,16 +44,13 @@ void kernel_main() {
     }
 
     //PRINTING S
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < height; j++) {
-            if ((i == 0 || i == height / 2
-                 || i == height - 1))
+    for (i = 0; i < h; i++) {
+        for (j = 0; j < h; j++) {
+            if ((i == 0 || i == h / 2 || i == h - 1))
                 print_str("*");
-            else if (i < height / 2
-                     && j == 0)
+            else if (i < h / 2 && j == 0)
                 print_str("*");
-            else if (i > height / 2
-                     && j == height - 1)
+            else if (i > h / 2 && j == h - 1)
                 print_str("*");
             else
                 print_str(" ");
@@ -64,11 +59,11 @@ void kernel_main() {
     }
 
     //PRINTING T
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < height; j++) {
+    for (i = 0; i < h; i++) {
+        for (j = 0; j < h; j++) {
             if (i == 0)
                 print_str("*");
-            else if (j == height / 2)
+            else if (j == h / 2)
                 print_str("*");
             else
                 print_str(" ");
@@ -77,87 +72,4 @@ void kernel_main() {
     }
 
 }
-
-// Function to print the pattern of 'N'
-void printN()
-{
-    int i, j, counter = 0;
-    for (i = 0; i < height; i++) {
-        print_str("*");
-        for (j = 0; j <= height; j++) {
-            if (j == height)
-                print_str("*");
-            else if (j == counter)
-                print_str("*");
-            else
-                print_str(" ");
-        }
-        counter++;
-        print_str("\n");
-    }
-}
-
-// Function to print the pattern of 'U'
-void printU()
-{
-    int i, j;
-    for (i = 0; i < height; i++) {
-        if (i != 0 && i != height - 1)
-            print_str("*");
-        else
-            print_str(" ");
-        for (j = 0; j < height; j++) {
-            if (((i == height - 1)
-                 && j >= 0
-                 && j < height - 1))
-                print_str("*");
-            else if (j == height - 1 && i != 0
-                     && i != height - 1)
-                print_str("*");
-            else
-                print_str(" ");
-        }
-        print_str("\n");
-    }
-}
-
-// Function to print the pattern of 'S'
-void printS()
-{
-    int i, j;
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < height; j++) {
-            if ((i == 0 || i == height / 2
-                 || i == height - 1))
-                print_str("*");
-            else if (i < height / 2
-                     && j == 0)
-                print_str("*");
-            else if (i > height / 2
-                     && j == height - 1)
-                print_str("*");
-            else
-                print_str(" ");
-        }
-        print_str("\n");
-    }
-}
-
-// Function to print the pattern of 'T'
-void printT()
-{
-    int i, j;
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < height; j++) {
-            if (i == 0)
-                print_str("*");
-            else if (j == height / 2)
-                print_str("*");
-            else
-                print_str(" ");
-        }
-        print_str("\n");
-    }
-}
-
 
